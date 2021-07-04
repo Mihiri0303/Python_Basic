4644ca92141c8fcd7a8b58ec673963b45638c97f
Mihiri0303 <93081@fhss.sjp.ac.lk>
2021-07-04 12:33:33 +0530

    UserInput_Operator

---
+++ UserInput_Operator.py
@@ -0,0 +1,59 @@
+# Add two userInputs
+# Method-01
+num1 = int(input("Enter number 01 : "))
+num2 = int(input("Enter number 02 : "))
+
+print(num1 + num2)
+
+# Method-02
+num1 = int(input("Enter number 01 : "))
+num2 = int(input("Enter number 02 : "))
+results = (num1 + num2)
+
+print(results)
+
+
+# Subtract two userInputs
+# Method-01
+num1 = int(input("Enter number 01 : "))
+num2 = int(input("Enter number 02 : "))
+
+print(num1 - num2)
+
+# Method-02
+num1 = int(input("Enter number 01 : "))
+num2 = int(input("Enter number 02 : "))
+results = (num1 - num2)
+
+print(results)
+
+
+# Divide two userInputs
+# Method-01
+num1 = int(input("Enter number 01 : "))
+num2 = int(input("Enter number 02 : "))
+
+print(num1 / num2)
+
+# Method-02
+num1 = int(input("Enter number 01 : "))
+num2 = int(input("Enter number 02 : "))
+results = (num1 / num2)
+
+print(results)
+
+
+# Multiply  two userInputs
+# Method-01
+num1 = int(input("Enter number 01 : "))
+num2 = int(input("Enter number 02 : "))
+
+print(num1 * num2)
+
+# Method-02
+num1 = int(input("Enter number 01 : "))
+num2 = int(input("Enter number 02 : "))
+results = (num1 * num2)
+
+print(results)
+
