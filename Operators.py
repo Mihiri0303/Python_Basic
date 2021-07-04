bf60713fc3531315ed453f785dae04812cfc611d
Mihiri0303 <93081@fhss.sjp.ac.lk>
2021-07-04 10:05:51 +0530

    Operators

---
+++ Operators.py
@@ -0,0 +1,72 @@
+# + Operator
+
+# Method-01
+print(50 + 20)
+
+# Method-02
+A = 50
+B = 20
+
+print(A + B)
+
+# Method-03
+A = 50
+B = 20
+C = (A + B)
+
+print(C)
+
+
+# - Operator
+
+# Method-01
+print(20-10)
+
+# Method-02
+a = 20
+b = 10
+
+print(a - b)
+
+# Method-03
+a = 20
+b = 10
+c = (a - b)
+
+print(c)
+
+
+# / Operator
+# Method-01
+print(100/5)
+
+# Method-02
+x = 100
+y = 5
+
+print(x / y)
+
+# Method-03
+x = 100
+y = 5
+r = (x / y)
+
+print(r)
+
+
+# * Operator
+# Method-01
+print(30 * 2)
+
+# Method-02
+X = 30
+Y = 2
+
+print(X * Y)
+
+# Method-03
+X = 30
+Y = 2
+R = (X * Y)
+
+print(R)
---
+++ userInput.py
@@ -0,0 +1,19 @@
+# User Inputs
+
+name = (input("Enter your name : "))
+Address = (input("Enter your Address : "))
+age = (input("Enter your age : "))
+
+# collect two user inputs
+num1 = int(input("Add number 01 : "))
+num2 = int(input("Add number 02 : "))
+Results = (num1 + num2)
+
+print(Results)
+
+
+
+
+
+
+
