6637d5dc51a7a9695078ade7210a00e17f2aa708
Mihiri0303 <93081@fhss.sjp.ac.lk>
2021-07-04 10:12:26 +0530

    userInputs

--- userInput.py
+++ userInput.py
@@ -5,14 +5,18 @@ Address = (input("Enter your Address : "))
 age = (input("Enter your age : "))
 
 # collect two user inputs
+# Method-01
+num1 = int(input("Add number 01 : "))
+num2 = int(input("Add number 02 : "))
+
+print(num1 + num2)
+
+# Method-02
 num1 = int(input("Add number 01 : "))
 num2 = int(input("Add number 02 : "))
 Results = (num1 + num2)
 
 print(Results)
-
-
-
 
 
 
