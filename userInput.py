2eb9bc3fc15d86151a0a7be4eb95a65507ca95bb
Mihiri0303 <93081@fhss.sjp.ac.lk>
2021-07-04 12:19:06 +0530

    userInputs

--- userInput.py
+++ userInput.py
@@ -3,20 +3,12 @@
 name = (input("Enter your name : "))
 Address = (input("Enter your Address : "))
 age = (input("Enter your age : "))
+first_name = (input("Enter your First name : "))
+last_name = (input("Enter your last name : "))
 
-# collect two user inputs
-# Method-01
-num1 = int(input("Add number 01 : "))
-num2 = int(input("Add number 02 : "))
 
-print(num1 + num2)
 
-# Method-02
-num1 = int(input("Add number 01 : "))
-num2 = int(input("Add number 02 : "))
-Results = (num1 + num2)
 
-print(Results)
 
 
 
