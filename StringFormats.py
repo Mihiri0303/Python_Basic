5e7f282e7e225ca075d1a037a61f437d5ad98b5d
Mihiri0303 <93081@fhss.sjp.ac.lk>
2021-07-06 15:55:59 +0530

    StringFormats

---
+++ StringFormats.py
@@ -0,0 +1,29 @@
+# Simple String Format
+
+
+# Single Quotation
+print('Hello Sri Lanka!')
+
+# Double Quotations
+print("Hello Sri Lanka!")
+
+
+# New Line
+print("Hello I am Mihiri.\nHow are you?")
+
+
+# Hide letters
+print("Python Basic")
+
+# Hide letter y
+print("Py\bthon Basic")
+
+# Hide letter s
+print("Python Bas\bic")
+
+
+# Customise string {}
+sample = "Time is {0} - Today is {1} - Month is{2} - Year is {3}"
+
+print(sample.format(12.12,"Monday","June",2021))
+
