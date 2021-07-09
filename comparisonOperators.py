a9a42958de9d58cdb4979e0880e0803f59bc02e8
Mihiri0303 <93081@fhss.sjp.ac.lk>
2021-07-09 13:17:58 +0530

    Comparison Operators

---
+++ comparisonOperators.py
@@ -0,0 +1,117 @@
+# (==) Operator
+# If it is equal
+A = 100
+B = 100
+
+print(A == B)
+
+# If it is not equal
+A = 10
+B = 100
+
+print(A == B)
+
+
+
+# (!=) Operator
+# If it is not equal
+A = 100
+B = 50
+
+print(A != B)
+
+# If it is equal
+A = 50
+B = 50
+
+print(A != B)
+
+
+# ( < ) Greater than or Less than
+# Greater than
+# If it is greater
+a = 500
+b = 100
+
+print(a > b)
+
+# If it is not greater
+a = 100
+b = 500
+
+print(a > b)
+
+
+# Less than
+# If it is less
+a = 100
+b = 500
+
+print(a < b)
+
+# If it is not less
+a = 500
+b = 50
+
+print(a < b)
+
+
+# (<= ) Greater or Less than or equal
+# Greater than or equal
+# If it is greater or equal
+a = 500
+b = 50
+
+print(a >= b)
+
+# If it is equal
+a = 50
+b = 50
+
+print(a >= b)
+
+# If it is not greater
+a = 50
+b = 100
+
+print(a >= b)
+
+# If it is not equal
+a = 50
+b = 100
+
+print(a >= b)
+
+
+# If it is less or equal
+a = 500
+b = 50
+
+print(a <= b)
+
+# If it is equal
+a = 50
+b = 50
+
+print(a <= b)
+
+# If it is not less
+a = 500
+b = 100
+
+print(a <= b)
+
+# If it is not equal
+a = 500
+b = 100
+
+print(a <= b)
+
+
+
+
+
+
+
+
+
