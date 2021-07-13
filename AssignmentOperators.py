5aef7c76b15fe3bb7b58175bbada9f343e44facf
Mihiri0303 <93081@fhss.sjp.ac.lk>
2021-07-13 18:27:14 +0530

    Assignment Operators

---
+++ AssignmentOperators.py
@@ -0,0 +1,63 @@
+# Assignment Operators
+# (=) Operator
+
+# to assign a value
+
+a = 100
+print(a)  # >>> 100
+
+a = 200
+print(a)  # >>> 200
+
+# (+=) Operator
+
+a += 10
+
+print(a)  # >>> 210
+
+# (-=) Operator
+
+a -= 20
+
+print(a)  # >>> 190
+
+# (*=) Operator
+
+a *= 2
+
+print(a)  # >>> 380
+
+# (/=) Operator
+
+a /= 2
+
+print(a)  # >>> 190.0
+
+# (//=) Operator
+
+a //= 2
+
+print(a)  # >>> 95.0
+
+# (%=) Operator
+
+a %= 3
+
+print(a)  # >>> 2.0
+
+# (**=) Operator
+
+a **= 2
+
+print(a)  # >>> 4.0
+
+
+
+
+
+
+
+
+
+
+
